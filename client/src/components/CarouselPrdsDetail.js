32a3aa4fb348fe283b0f6774b2193d63d35b6687
hungUET231 <hungngv231@gmail.com>
2020-12-08 00:09:35 +0700

    fix product image gallery responsive

--- client/src/components/CarouselPrdsDetail.js
+++ client/src/components/CarouselPrdsDetail.js
@@ -1,20 +1,20 @@
 import React from "react";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
+import "./CarouselPrdsDetail.css";
 
 function CarouselPrdsDetail({ photos }) {
   const images = photos.map((photo) => {
     return { original: photo, thumbnail: photo };
   });
   return (
-    <div className="gallery-product">
-      <ImageGallery
-        lazyLoad
-        thumbnailPosition="left"
-        showBullets
-        items={images}
-      />
-    </div>
+    <ImageGallery
+      className="gallery-product"
+      lazyLoad
+      thumbnailPosition="left"
+      showBullets
+      items={images}
+    />
   );
 }
 
